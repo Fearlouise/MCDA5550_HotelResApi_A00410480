8f45eeaaedb95bac748ca857dd065d21d1b4b25c
Fearlouise <louise.fear@hotmail.com>
2025-04-11 23:40:36 -0300

    Final changes for deployment

--- src/main/java/com/example/MCDAHotelApi/wrapper/GuestListWrapper.java
+++ src/main/java/com/example/MCDAHotelApi/wrapper/GuestListWrapper.java
@@ -1,18 +1,15 @@
 package com.example.MCDAHotelApi.wrapper;
 
 import com.example.MCDAHotelApi.model.GuestModel;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class GuestListWrapper {
     private List<GuestModel> guests;
-
-    public GuestListWrapper() {
-    }
-
-    public GuestListWrapper(List<GuestModel> guests) {
-        this.guests = guests;
-    }
 }
